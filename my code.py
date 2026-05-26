7720a0333ba450e7ccecbe4aa389b0661a55c592
Manish <manishjaiswar1221@gmail.com>
2026-05-26 17:01:43 +0530

    Third veersion of code done

--- my code.py
+++ my code.py
@@ -13,9 +13,9 @@ df=pd.DataFrame(data)
 new_row_loc = {'Name': 'V2', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
-# # Adding new row to df for V3
-# new_row={'Name':'Usagi','Age':30,'City':'City2'}
-# df.loc[len(df.index)]=new_row
+# Adding new row to df for V3
+new_row={'Name':'Usagi','Age':30,'City':'City2'}
+df.loc[len(df.index)]=new_row
 
 data_dir="data"
 os.makedirs(data_dir,exist_ok=True)
